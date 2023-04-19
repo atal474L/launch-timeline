e88c6ba432a56b3ce3b2ec07ab61cef9f26f73c2
Atal Abdulkhalil <atal.abdulkhalil@student.odisee.be>
2023-04-19 04:46:46 +0200

    created new laravel project, added models

---
+++ app/Models/Checklist.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Checklist extends Model
+{
+    use HasFactory;
+
+    public function projects(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
+class Project extends Model
+{
+    use HasFactory;
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function phases(): HasMany
+    {
+        $this->hasMany(Phase::class);
+    }
+}
