576254862e23f65e2fc6eb464577bfe11a90970b
Atal Abdulkhalil <atal.abdulkhalil@student.odisee.be>
2023-05-17 01:22:47 +0200

    project filter adjusted

--- launch/src/views/Projects.jsx
+++ launch/src/views/Projects.jsx
@@ -1,29 +1,47 @@
 import { Button, Container, Stack } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import ProjectTable from "../components/ProjectTable";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import Search from "../components/Search";
+import { InputText } from "primereact/inputtext";
+import { FilterMatchMode } from "primereact/api";
+import { Dropdown } from "primereact/dropdown";
 
 //theme
 import "primereact/resources/themes/lara-light-indigo/theme.css";
-
 //core
 import "primereact/resources/primereact.min.css";
 
 export default function Projects() {
   const [projects, setProjects] = useState([]);
+  const [phaseNameOptions, setPhaseNameOptions] = useState([
+    "Post CMS training",
+    "Pre Pre-launch",
+    "Pre-launch",
+    "Post launch",
+    "Offboarding",
+  ]);
+  const [selectedPhaseName, setSelectedPhaseName] = useState();
+  const [filters, setFilters] = useState();
 
-  const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(5);
+  const onPhaseNameFilterChange = (e) => {
+    setSelectedPhaseName(e.value);
+    setFilters({
+      global: {
+        value: e.value,
+        matchMode: FilterMatchMode.EQUALS,
+      },
+    });
+  };
 
-  const getPage = function (page, limit) {
-    let array = [];
-    for (let i = (page - 1) * limit; i < page * limit; i++) {
-      array.push(projects[i]);
-    }
-    return array;
+  const setGlobalFilters = (e) => {
+    setFilters({
+      global: {
+        value: e.target.value,
+        matchMode: FilterMatchMode.CONTAINS,
+      },
+    });
+    debugger;
   };
 
   useEffect(() => {
@@ -41,7 +59,6 @@ export default function Projects() {
         console.error("Error:", error);
       });
   }, []);
-
   return (
     <>
       <Container>
@@ -62,21 +79,46 @@ export default function Projects() {
       </Container>
 
       <div className="container homeSec2">
-        <Container className="homeTable">
-          {/* {projects && <ProjectTable projects={getPage(1, 3)}></ProjectTable>} */}
-        </Container>
-
+        {/* <Container className="homeTable">
+          
+        </Container> */}
+        <InputText onInput={setGlobalFilters}></InputText>
+        <Dropdown
+          value={selectedPhaseName}
+          options={phaseNameOptions}
+          onChange={onPhaseNameFilterChange}
+          placeholder="Select Phase Name"
+          className="p-column-filter"
+          showClear
+        />
         <DataTable
           value={projects}
           paginator
           rows={3}
           rowsPerPageOptions={[5, 10, 25, 50]}
           className="table"
+          filters={filters}
         >
           <Column field="name" header="Projectnaam" sortable></Column>
-          <Column
+          {/* <Column
             field={(rowData) => rowData?.phases[0]?.phase_name}
             header="Actieve fase"
+          /> */}
+
+          <Column
+            field={(rowData) => rowData?.phases[0]?.phase_name}
+            header="Phase Name"
+            filter
+            filterElement={
+              <Dropdown
+                value={selectedPhaseName}
+                options={phaseNameOptions}
+                onChange={onPhaseNameFilterChange}
+                placeholder="Select Phase Name"
+                className="p-column-filter"
+                showClear
+              />
+            }
           />
           <Column
             field="active_phase_deadline"
