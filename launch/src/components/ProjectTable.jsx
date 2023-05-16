253caebbec33a8ac9af5bc74d34c6236f0c450db
Atal Abdulkhalil <atal.abdulkhalil@student.odisee.be>
2023-05-16 23:05:40 +0200

    sorting, pagination and filter functionalities added for projects overview page

--- launch/src/components/ProjectTable.jsx
+++ launch/src/components/ProjectTable.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { Link } from "react-router-dom";
 
 export default function ProjectTable(props) {
+  const [data, setData] = useState(props.projects);
+  const [order, setOrder] = useState("ASC");
+
+  useEffect(() => {
+    setData(props.projects);
+    console.log(data);
+  }, [props]);
+
+  const sorting = (col) => {
+    if (order === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("DESC");
+    }
+
+    if (order === "DESC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("ASC");
+    }
+  };
+
   return (
     <Table>
       <thead>
         <tr>
-          <th>Projectnaam</th>
+          <th onClick={() => sorting("name")}>Projectnaam</th>
           <th>Actieve fase</th>
           <th>Fasedeadline</th>
           <th>Progressie</th>
@@ -16,23 +42,31 @@ export default function ProjectTable(props) {
         </tr>
       </thead>
       <tbody>
-        {props.projects.map((project) => (
-          <tr key={project.id}>
-            <td>{project.name}</td>
-            <td>{project.phases[0].phase_name}</td>
-            <td>{project.active_phase_deadline}</td>
-            <td>{project.progress} %</td>
-            <td>{project.state}</td>
-            <td>
-              <Link
-                to={`api/projects/${project.id}/details`}
-                className="primaryLink"
-              >
-                Project bekijken
-              </Link>
-            </td>
+        {data.length > 0 ? (
+          data.map((project, index) =>
+            project && index !== undefined ? (
+              <tr key={`project_${index}`}>
+                <td>{project.name}</td>
+                <td>{project?.phases[0]?.phase_name}</td>
+                <td>{project?.active_phase_deadline}</td>
+                <td>{project?.progress}</td>
+                <td>{project?.state}</td>
+                <td>
+                  <Link
+                    to={`api/projects/${project?.id}/details`}
+                    className="primaryLink"
+                  >
+                    Project bekijken
+                  </Link>
+                </td>
+              </tr>
+            ) : null
+          )
+        ) : (
+          <tr>
+            <td colSpan={6}>No projects available</td>
           </tr>
-        ))}
+        )}
       </tbody>
     </Table>
   );
--- launch/src/views/Projects.jsx
+++ launch/src/views/Projects.jsx
@@ -1,3 +1,102 @@
+import { Button, Container, Stack } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ProjectTable from "../components/ProjectTable";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import Search from "../components/Search";
+
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+
+//core
+import "primereact/resources/primereact.min.css";
+
 export default function Projects() {
-  return <div> jaaj </div>;
+  const [projects, setProjects] = useState([]);
+
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+
+  const getPage = function (page, limit) {
+    let array = [];
+    for (let i = (page - 1) * limit; i < page * limit; i++) {
+      array.push(projects[i]);
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/projectoverview")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch the projects");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Stack direction="horizontal" className="home">
+          <h1>Projecten overzicht</h1>
+
+          <div className="ms-auto deadline">
+            <Link
+              // to={`/api/project/${
+              //   todaysProject ? todaysProject[0]?.project_id : null
+              // }/${activePhaseId}`}
+              className="btn btn-primary secondryBtnLink"
+            >
+              Nieuw project aanmaken
+            </Link>
+          </div>
+        </Stack>
+      </Container>
+
+      <div className="container homeSec2">
+        <Container className="homeTable">
+          {/* {projects && <ProjectTable projects={getPage(1, 3)}></ProjectTable>} */}
+        </Container>
+
+        <DataTable
+          value={projects}
+          paginator
+          rows={3}
+          rowsPerPageOptions={[5, 10, 25, 50]}
+          className="table"
+        >
+          <Column field="name" header="Projectnaam" sortable></Column>
+          <Column
+            field={(rowData) => rowData?.phases[0]?.phase_name}
+            header="Actieve fase"
+          />
+          <Column
+            field="active_phase_deadline"
+            header="Fasedeadline"
+            sortable
+          ></Column>
+          <Column field="progress" header="Progressie"></Column>
+          <Column field="state" header="Project status" sortable></Column>
+          <Column
+            body={(rowData) => (
+              <Link
+                to={`api/projects/${rowData?.id}/details`}
+                className="primaryLink"
+              >
+                Project bekijken
+              </Link>
+            )}
+          />
+        </DataTable>
+      </div>
+    </>
+  );
 }
